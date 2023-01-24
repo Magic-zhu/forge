ef3d983364fcf05879da95061912c118dc867b5b
magic-zhu <18817313320@163.com>
2023-01-24 20:34:27 +0800

    update drag

--- src/views/BluePrintEditor/blueprint.vue
+++ src/views/BluePrintEditor/blueprint.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="container">
+  <div class="container" @drop="drop" @dragover="dragover">
     <div></div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import {defineComponent} from 'vue';
 export default defineComponent({
   setup() {
-    return {};
+    const drop = (ev: DragEvent) => {
+      ev.preventDefault();
+      console.log(ev.dataTransfer?.getData('varible'));
+    };
+    const dragover = (ev: DragEvent) => {
+      ev.preventDefault();
+    };
+    return {
+      drop,
+      dragover,
+    };
   },
 });
 </script>
 <style lang="less" scoped>
-@import "./header.less";
+@import './blueprint.less';
 </style>
--- src/views/BluePrintEditor/header.vue
+++ src/views/BluePrintEditor/header.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import {defineComponent} from 'vue';
 export default defineComponent({
   setup() {
     return {};
@@ -12,5 +12,5 @@ export default defineComponent({
 });
 </script>
 <style lang="less" scoped>
-@import "./header.less";
+@import './header.less';
 </style>
