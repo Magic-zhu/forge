a287ab9df19f333e2d56f85e64d65d4f490a78a7
magic-zhu <18817313320@163.com>
2023-01-26 22:19:00 +0800

    update

--- src/views/BluePrintEditor/blueprint.vue
+++ src/views/BluePrintEditor/blueprint.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="container" @drop="drop" @dragover="dragover">
-    <div></div>
+    <div ref="app" id="app"></div>
   </div>
 </template>
 <script lang="ts">
 import {defineComponent} from 'vue';
+declare const window: any;
 export default defineComponent({
   setup() {
+    let Editor: any;
+    const createCanvas = () => {
+      Editor = new window.blueprint.BluePrintEditor();
+    };
+    console.log('blueprint', window.blueprint);
     const drop = (ev: DragEvent) => {
       ev.preventDefault();
       console.log(ev.dataTransfer?.getData('varible'));
