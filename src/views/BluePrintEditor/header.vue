676abc569b3ddb977ebc97b92e222923e7db34d4
magic-zhu <18817313320@163.com>
2023-01-27 11:01:52 +0800

    🚧 work in process

--- src/views/BluePrintEditor/header.vue
+++ src/views/BluePrintEditor/header.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="container">
-    <div></div>
+    <div>
+      <div class="fn">
+        <Icon type="fa-play" color="#57d97c" />
+        <Span class="label">Run</Span>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
+import Icon from "@components/Icon/Icon.vue";
 import { defineComponent } from "vue";
 export default defineComponent({
   setup() {
     return {};
   },
+  components: { Icon },
 });
 </script>
 <style lang="less" scoped>
